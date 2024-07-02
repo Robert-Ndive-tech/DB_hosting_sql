d271458b50940efc1e6ef060bfef2b8db9b178fb
robert <robertndive29@gmail.com>
2024-07-02 23:02:10 +0100

    n

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ require('dotenv').config()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-const accountRouter = require('./routes/account.router')
 const postsRouter = require('./routes/posts.router')
 const authRouter = require('./routes/auth.router')
 
 app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/accounts", accountRouter)
 app.use("/api/v1/auth", authRouter)
 
 const PORT = process.env.PORT || 1000
