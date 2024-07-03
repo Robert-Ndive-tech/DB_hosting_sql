30731dd9e61540a869fb90a66c6494ac668a2714
robert <robertndive29@gmail.com>
2024-07-03 08:05:59 +0100

    fix

--- routes/account.router.js
+++ routes/account.router.js
@@ -10,7 +10,6 @@ router.post("/", accountController.create)
 router.put("/:id", accountController.update)
 router.get("/:id", accountController.getById)
 router.delete("/:id", accountController.delete)
-router.login("/login",accountController.login)
-router.register("/register",accountController.register)
+//router.login("/",accountController.login) router.register("/",accountController.register)
 
 module.exports = router
