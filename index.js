3b9476e1c4c3af16883af56f7e49937fa9377912
robert <robertndive29@gmail.com>
2024-07-04 22:52:44 +0100

    na

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require('cors');
 
 //Kizdarodino database hostings
 require('dotenv').config()
 
+app.use(cors());
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
