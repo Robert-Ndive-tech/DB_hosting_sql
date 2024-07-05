934383b1bb3c74fc7a7fcbd7ecb358896385a961
robert <robertndive29@gmail.com>
2024-07-05 04:46:58 +0100

    a

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express")
 const app = express()
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const fs = require('fs');
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up multer for file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
 
 //Kizdarodino database hostings
 require('dotenv').config()
@@ -30,6 +27,29 @@ const shelterRouter = require('./routes/shelter.router')
 const LocationRouter = require('./routes/Location.router')
 const imageRouter = require('./routes/router/image.router')
 const specialRouter = require('./routes/special.router')
+
+
+
+const multer = require("multer");
+var fs = require("fs");
+
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "../client/src/Assets"); // Change 'uploads/' to your desired directory path
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+  });
+
+
+  const upload = multer({ storage });
+  
+
+
+
 
 
 app.use("/api/v1/posts", postsRouter)
