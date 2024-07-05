82f9c3bd4d3719292fc6433ee626c567d415aa21
robert <robertndive29@gmail.com>
2024-07-06 00:46:42 +0100

    g

--- routes/Location.router.js
+++ routes/Location.router.js
@@ -2,11 +2,16 @@
 const express = require("express")
 const LocationController = require("../controller/LocationDetails")
 const router = express.Router()
+const multer = require("multer");
+var fs = require("fs");
 
+const Upload = multer({ dest: 'uploads/' });
 
 router.post("/",LocationController.create)
 router.put("/create",LocationController.update)
 router.get("/find",LocationController.getById)
 router.get("/list",LocationController.getAll)
+router.post("/send",Upload.single('image'),LocationController.send)
+router.get("/fetch/:id",LocationController.fetch)
 
 module.exports = router
