5a4ed35e5dba09e1a7a1d2f7407b1ae012c1fc41
robert <robertndive29@gmail.com>
2024-07-05 02:08:52 +0100

    ja

--- routes/router/image.router.js
+++ routes/router/image.router.js
@@ -1,11 +1,10 @@
 //Kizdarodino database hostings
 const express = require("express")
-const imageController = require("../../Scontroller/image.controller")
+
+
 const router = express.Router()
-
-
+const imageController = require("../../Scontroller/image.controller")
 
 router.post("/",imageController)
-
 
 module.exports = router
