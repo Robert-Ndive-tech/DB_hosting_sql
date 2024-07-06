c4dd9445f62fbed15552d4a1b726d3e0a5c43f70
robert <robertndive29@gmail.com>
2024-07-06 18:51:52 +0100

    x

--- routes/disastercenter.router.js
+++ routes/disastercenter.router.js
@@ -1,19 +1,6 @@
 //Kizdarodino database hostings
 const express = require("express")
 const router = express.Router()
-const multer = require("multer");
-var fs = require("fs");
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "../client/src/Assets"); // Change 'uploads/' to your desired directory path
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    },
-  });
-
-  const upload = multer({ storage });
 
 
 const disasterController = require("../controller/disastercenter.controller")
@@ -25,7 +12,7 @@ router.put("/change", disasterController.update)
 router.get("/find", disasterController.getById)
 router.delete("/delete", disasterController.delete)
 router.get("/get", disasterController.recieve)
-router.post("/upload",upload.single('photo'), disasterController.send)
+router.post("/upload", disasterController.send)
 router.get("/get/:id", disasterController.recievebyid)
 
 module.exports = router
